5c96f109f4318414c69ca584512382b86751e27e
Astrid Febrianca <astridstr24@gmail.com>
2018-04-27 14:26:18 +0700

    Add files via upload

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+$plugin->version = 2017111301;
+$plugin->component = 'report_myreport';
+$plugin->requires = 2017111300;
+$plugin->release = '1.0 (Build: 2018041800)';
+$plugin->maturity = MATURITY_STABLE;
